58e75f5c8ebb5a5bb51b7efe6ff9f00f983238b6
Pradeep B <{ID}+{username}@users.noreply.github.com>
2021-05-24 23:14:13 +0530

    Hello world working

---
+++ src/main/java/com/pradeep/restfulservices/restfulservicesdemo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.pradeep.restfulservices.restfulservicesdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class HelloWorldController {
+
+	// This function shall handle the GET request to the /hello-world path
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world") // This is a generic way
+	@GetMapping( path="/hello-world") // Instead, we can use a specific GetMapping that maps a GET call to this method
+	public String helloWorld()
+	{
+		return "Hello World, I am a Spring boot app";
+	}
+}
