c86e21d15ac05a216bac82ba09fc48c85cee8886
Pradeep B <{ID}+{username}@users.noreply.github.com>
2021-09-12 10:18:38 +0530

    Using ServletUriComponentsBuilder to return the created user URI

--- src/main/java/com/pradeep/restfulservices/restfulservicesdemo/user/UserResource.java
+++ src/main/java/com/pradeep/restfulservices/restfulservicesdemo/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.pradeep.restfulservices.restfulservicesdemo.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -32,9 +35,17 @@ public class UserResource {
 	
 	// Create a new user
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user)
+	public ResponseEntity<Object> createUser(@RequestBody User user)
 	{
+		
 		User savedUser = uds.save(user);
-		return savedUser;
+
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId())
+		.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
